127127c153283391961e95e54e24542aaf276cc5
EJ Perez <idyey04@gmail.com>
2018-09-06 22:24:16 +0800

    Initial pwa rebuild

--- src/main.js
+++ src/main.js
@@ -3,45 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import axios from 'axios'
 
-Vue.config.productionTip = false;
-
-if ( process.env.NODE_ENV === 'production' ) {
-	window.apiHost = 'https://ejcifra-api.herokuapp.com/api'
-} else {
-	window.apiHost = 'http://ejcifra-api/api'
-}
-
-window.axios = axios;
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue( {
-	el: '#app',
-	router,
-	components: { App },
-	template: '<App/>'
-} )
-
-/* Register the service worker */
-if ( 'serviceWorker' in navigator ) {
-	window.addEventListener( 'load', function () {
-		navigator.serviceWorker.register( 'static/sw.js' ).then(
-			function ( registration ) {
-				// Registration was successful
-				console.log( 'ServiceWorker registration successful with scope: ', registration.scope );
-			},
-			function ( err ) {
-				// registration failed :( 
-				console.log( 'ServiceWorker registration failed: ', err );
-			} );
-	} );
-}
-
-// include ChordPlus
-let chordPlusScript = document.createElement( "script" );
-chordPlusScript.setAttribute(
-	"src",
-	"static/vendor/chord-plus/dist/chord-plus.min.js"
-);
-document.head.appendChild( chordPlusScript );
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
