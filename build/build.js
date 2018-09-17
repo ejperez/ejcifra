719ca579f0cd70cda3315efa987fefe973f23400
EJ Perez <idyey04@gmail.com>
2018-09-17 14:38:43 +0800

    Changed /static to static

--- build/build.js
+++ build/build.js
@@ -11,9 +11,13 @@ const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
+const shell = require('shelljs')
 
 const spinner = ora('building for production...')
 spinner.start()
+
+shell.rm('-rf', 'docs/*')
+console.log(chalk.cyan('  Cleaned docs.\n'))
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
@@ -32,6 +36,12 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
-    ))
+	))
+
+	shell.cp('-rf', 'dist/*', 'docs')
+	console.log(chalk.cyan('  Copied content of dist to docs.\n'))
+	
+	shell.sed('-i', '/static', 'static', 'docs/index.html')
+	console.log(chalk.cyan('  Modified index.html.\n'))
   })
 })
